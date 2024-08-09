aeeadcd7a48b20d140dda436a65fda92d2bd3f6a
Mahmut Can SARIBAL <mahmutcansaribal_19@hotmail.com>
2024-08-09 16:41:49 +0300

    add requriments.txt

---
+++ LibraryCheckVersion.py
@@ -0,0 +1,12 @@
+import flask
+import cv2
+import face_recognition
+import sqlite3
+import PIL
+import numpy as np
+
+print("Flask version:", flask.__version__)
+print("OpenCV version:", cv2.__version__)
+print("face_recognition version:", face_recognition.__version__)
+print("Pillow version:", PIL.__version__)
+print("NumPy version:", np.__version__)
